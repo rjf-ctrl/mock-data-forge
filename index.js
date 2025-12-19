d4be84d699bb0e58c992c5d2622abfa1c81fbc4c
rjf-ctrl <rejafairooz@gmail.com>
2025-12-19 20:09:10 +0530

    Added a json file for names, phone numbers and emails and included it in the genrator

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-const names = ["Alice", "Bob", "Charlie", "Diana"];
+const fs = require("fs");
+const name = JSON.parse(fs.readFileSync("names.json", "utf-8"));
+
+
+function randomPerson() {
+    return name[Math.floor(Math.random() * name.length)];
+}
+
+
+function randomString(length = 6) {
+  const chars = "abcdefghijklmnopqrstuvwxyz";
+  let str = "";
+
+  for (let i = 0; i < length; i++) {
+    str += chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  return str;
+}
+
+
+function randomInteger(min = 0, max = 100) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomFloat(min = 0, max = 10, decimals = 2) {
+  const num = Math.random() * (max - min) + min;
+  return Number(num.toFixed(decimals));
+}
+
 
 function generateData(schema) {
     const result = {};
@@ -10,12 +39,44 @@ function generateData(schema) {
             result[field] = Math.random() <0.5;
         }
 
-        if (type === "name") {
-            result[field] = names[Math.floor(Math.random() * names.length)];
+        if (type === "first name") {
+            result[field] = randomPerson().First;
+        }
+
+        if (type === "last name") {
+            result[field] = randomPerson().Last;
+        }
+
+        if (type === "middle name") {
+            result[field] = randomPerson().Mid;
+        }
+
+        if (type === "full name") {
+            result[field] = randomPerson().Full;
         }
 
         if (type === "uuid") {
         result[field] = Math.random().toString(36).substring(2,10);
+        }
+
+        if (type === "string"){
+            result[field] = randomString();
+        }
+
+         if (type === "integer") {
+            result[field] = randomInteger();
+        }
+
+        if (type === "float") {
+            result[field] = randomFloat();
+        }
+
+        if (type === "email") {
+            result[field] = randomPerson().Email;
+        }
+
+        if (type === "phone") {
+            result[field] = randomPerson().Phone;
         }
 
     } return result;
@@ -23,8 +84,17 @@ function generateData(schema) {
 
 const schema = {
     id: "uuid",
-    full_name: "name",
+    age: "integer",
+    decimal: "float",
+    gibberish: "string",
+    first_name: "first name",
+    last_name: "last name",
+    middle_name: "middle name",
+    full_name: "full name",
+    email: "email",
+    phone: "phone",
     is_active: "boolean",
+
 };
 
 const output = generateData(schema);
